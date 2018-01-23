0bd7e7ed9d5a2bdfd1e335a9cebd0d1d0b48dc8d
Lawrence Chan <chanlaw@seas.upenn.edu>
2018-01-22 21:58:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup,find_packages
 setup(
     name='rllab',
     install_requires=['theano', 'cached-property', 'python-dateutil', 
-'path.py', 'joblib', 'pyprind'],
+'path.py', 'joblib', 'pyprind', 'dill'],
     packages=[package for package in find_packages()
                 if package.startswith('rllab')],
     version='0.1.0',
