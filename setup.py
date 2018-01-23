7ebbc9b5df3815ea38600d8b7b9a78ec32d114ba
Lawrence Chan <chanlaw@seas.upenn.edu>
2018-01-22 21:36:31 -0500

    update setup to require dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup,find_packages
 
 setup(
     name='rllab',
+    install_requires=['theano', 'cached-property', 'python-dateutil', 
+'path.py', 'joblib', 'pyprind'],
     packages=[package for package in find_packages()
                 if package.startswith('rllab')],
     version='0.1.0',
