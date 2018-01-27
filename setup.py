1997ab0cb8ef2f8cb7bb36e2a163d1308c144fce
Lawrence Chan <chanlaw@seas.upenn.edu>
2018-01-27 17:23:07 -0500

    add lasagne to rllab requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup,find_packages
 setup(
     name='rllab',
     install_requires=['theano', 'cached-property', 'python-dateutil', 
-'path.py', 'joblib', 'pyprind', 'dill'],
+'path.py', 'joblib', 'pyprind', 'dill', 'lasagne'],
     packages=[package for package in find_packages()
                 if package.startswith('rllab')],
     version='0.1.0',
